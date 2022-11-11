0df9bc7c1712108f27054038da9b4b3a5c4cf5b4
cdstep02 <84064588+cdstep02@users.noreply.github.com>
2022-11-11 09:37:53 -0500

    Add files via upload

---
+++ LLUList.cpp
@@ -0,0 +1,175 @@
+// Implementation file for AUList
+#include <iostream>
+#include "LLUList.h"
+#include <cmath>
+
+struct LNode {
+    int item;
+    LNode* next;
+};
+
+LLUList::LLUList() {
+  ListStart=NULL;  
+}
+
+LLUList::~LLUList() {
+  //Note: The destructor can have compability issues with the assignment operator in returns.  It is recommended for HW #1 that you keep the code commented out 
+  /*
+  LNode* tempPtr; 
+  while (ListStart != NULL) {
+    tempPtr = ListStart;
+    ListStart = ListStart->next;
+    delete tempPtr;
+  }
+  */
+}
+
+bool LLUList::IsFull() const {
+  LNode* testNode;
+  try {
+    testNode = new LNode;
+    delete testNode;
+    return false;
+  }
+  catch(std::bad_alloc exception) {
+    return true;
+  }
+}
+
+int LLUList::GetLength() const {
+  LNode* LenPos = ListStart;
+  int length=0;
+  while (LenPos != NULL) {
+	LenPos=LenPos->next;
+	length++;
+  }
+  return length;
+};
+
+int LLUList::GetItem(int gitem) { //Procedure is similar to that of AUList, although the relevant data is stored different.
+								  //also, it's better for LLUList to proceed in order from the beginning
+   int position=0;
+   LNode* nodeIter;
+   nodeIter=ListStart;
+   while ((nodeIter != NULL) && (nodeIter->item !=gitem)) {
+      nodeIter=nodeIter->next;
+      position++; }
+   if (nodeIter==NULL)
+      return -1;
+   else
+      return position;
+}
+
+void LLUList::MakeEmpty() { //Unlike the AUList implementation, we need to step through and delete EVERY node
+  LNode* tempPtr;
+  while (ListStart != NULL) {
+    tempPtr = ListStart;
+    ListStart = ListStart->next;
+    delete tempPtr;
+  }  
+}
+
+
+void LLUList::PutItem(int newitem) {
+  LNode* newNode; //Get a pointer 			
+  newNode = new LNode; //Create a new node. 
+  newNode->item = newitem; // Store the item in the node
+  newNode->next = ListStart; // Store address of next node 
+  ListStart = newNode; // Redirect start of list
+}
+
+void LLUList::DeleteItem(int ditem) { //A little bit tricky because we need to "scout ahead" and relink the object
+									  //before the deleted object to the one after. 
+  LNode *tmpNode, *nodeIter;
+  if (ListStart->item == ditem) {
+    tmpNode=ListStart;
+    ListStart=ListStart->next;
+  } 
+  else{
+    nodeIter=ListStart;
+    while ((nodeIter->next->item !=ditem)) 
+      nodeIter=nodeIter->next;
+    tmpNode=nodeIter->next;
+    nodeIter->next=(nodeIter->next)->next; 
+  }
+  delete(tmpNode);
+}
+
+
+void LLUList::ResetList() {
+  curPos=ListStart;
+}
+
+int LLUList::GetNextItem() {
+  int myitem;
+  if (curPos == NULL) //The implemented option throw an errors if someone tries to access an item past the last one. 
+    throw(38); //EOS Exception
+  myitem = curPos->item;
+  curPos=curPos->next;
+  return myitem;
+};
+
+void LLUList::PrintList() {
+  LNode* printPtr;
+  printPtr=ListStart;
+  std::cout<<"(";
+  while (printPtr != NULL) {
+    std::cout<<printPtr->item;
+    if (printPtr->next!=NULL)
+	  std::cout<<", ";
+	printPtr=printPtr->next;
+  }    
+  std::cout<<")"<<std::endl;
+}
+
+double LLUList::getMean()
+{
+	double sum = 0;
+	double mean = 0;
+	LNode* itemPtr;
+	itemPtr = ListStart;
+	while(itemPtr != NULL)
+	{
+		sum += itemPtr->item;
+		itemPtr = itemPtr->	next;
+	}
+	mean = sum / GetLength();
+	
+	return mean;
+} 
+
+double LLUList::getStdev()
+{
+	double intermediate = 0;
+	LNode* itemPtr;
+	itemPtr = ListStart;
+	double step2;
+	double step3;
+	double standardDev;
+	while(itemPtr != NULL)
+	{
+		intermediate += pow(itemPtr->item - getMean(), 2);
+		itemPtr = itemPtr->next;
+	}
+	standardDev = sqrt(intermediate / GetLength());
+	return standardDev;
+}
+
+LLUList LLUList::DuplicateNV(int n)
+{
+	LLUList duplicateL;
+	LNode* itemPtr;
+	itemPtr = ListStart;
+	int i = 0;
+	while(itemPtr != NULL && i < n)
+	{
+		duplicateL.PutItem(itemPtr->item);
+		itemPtr = itemPtr->next;
+		++i;
+	}
+	
+	return duplicateL;
+}
+
+
+
---
+++ LLUList.h
@@ -0,0 +1,41 @@
+struct LNode;
+
+//Remember that header files should contain DECLARATIONS, but not DEFINITIONS for the associated class functions.
+class LLUList
+{
+public:
+  
+  LLUList(); // Constructor
+  
+  ~LLUList(); // Destructor
+  
+  void MakeEmpty();  // Returns the list to the empty state.
+    
+  bool IsFull() const; //Determines whether list is full. (Precondition -- list has been initialized).  Return value of "True" indicates class is full.
+  
+  int GetLength() const; //Determines the number of elements in list.
+
+  int GetItem(int); //Retrieves position of list element matching input item (if present).  If not present, -1 is returned.
+
+  void PutItem(int); //Adds the item to the list.
+  
+  void DeleteItem(int); //Deletes the element whose key matches item's key.
+  
+  void ResetList();  //Initializes iterator's current position for an iteration through the list.
+  
+  void PrintList();  //Print all elements of the list in a readable format.
+  
+  int GetNextItem(); //Gets the next element in the list. 
+  
+  double getMean(); //calculates and returns a mean value
+  
+  double getStdev(); //calculates and returns the standard deviation
+  
+  LLUList DuplicateNV(int); //duplicates the first N values of the Linked List and puts them into a new list
+  
+  
+private:
+  LNode* ListStart;  
+  LNode* curPos;
+};
+
